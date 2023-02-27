5d92735f0c74880e5da9fce14cb4cd6e938dae8b
Koichi Kiyokawa <koichi20110068@gmail.com>
2023-02-28 00:06:11 +0900

    fix: fix response body type is never

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,5 @@
 export type Get<O, Keys extends unknown[]> = Keys extends [infer Key, ...infer Rest]
   ? Key extends keyof O
-    ? Get<O[Key], Rest>
+    ? Get<Required<O>[Key], Rest>
     : never
   : O;
